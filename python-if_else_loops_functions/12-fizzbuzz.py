73fb997372d5e8d092e72fefc9af49566c5c0e2a
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-09 16:10:02 +0200

    functions

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(101):
-        if num % 3 == 0:
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 != 0:
             print("Fizz", end=" ")
-        elif num % 5 == 0:
+        elif num % 5 == 0 and num % 3 != 0:
             print("Buzz", end=" ")
         elif num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz", end=" ")

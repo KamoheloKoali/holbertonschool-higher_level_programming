c5a8e1f1a756cc10744c7e6e20d1752203a9fbb0
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-09 15:38:31 +0200

    functions

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        upper_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print("{:s}".format(upper_char), end='')
+    print()
+uppercase("Best School 98 Battery street")

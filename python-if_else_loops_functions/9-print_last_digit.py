eb77b5403559319d2ea2e5f5e8ba82723d1873b7
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-09 15:47:57 +0200

    functions

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,5 @@ def print_last_digit(number):
     last_num = abs(number) % 10
     if number < 0:
         last_num = -abs(last_num)
+    print(last_num)
     return last_num
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)

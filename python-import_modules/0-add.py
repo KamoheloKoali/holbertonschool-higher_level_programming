85670b06a00788bc4e5fa94303ad6ad8d9b2b107
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-11 11:32:41 +0200

    import module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{first_val} + {sec_val} = {result}".format(first_val=a, sec_val=b, result=add(a, b)))
+    print("{fir} + {sec} = {res}".format(fir=a, sec=b, res=add(a, b)))

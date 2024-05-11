60357db4a7f472c2eaf0f18b3bb1258430ff11b5
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-11 11:29:04 +0200

    import module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{a} + {b} = {add(a, b)}".format(a = a, b = b, add(a, b)))
+    print("{a} + {b} = {add}".format(a = a, b = b, add = add(a, b)))

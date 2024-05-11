cd934aa0e8d33b82f3a532d9ab21603ff1dafa1d
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-11 12:03:38 +0200

    import module

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name == "__main__":
+    import calculator_1 as cal
+    a = 10
+    b = 5
+    print("{fir} + {sec} = {res}".format(fir=a, sec=b, res=cal.add(a, b)))
+    print("{fir} - {sec} = {res}".format(fir=a, sec=b, res=cal.sub(a, b)))
+    print("{fir} * {sec} = {res}".format(fir=a, sec=b, res=cal.mul(a, b)))
+    print("{fir} / {sec} = {res}".format(fir=a, sec=b, res=cal.div(a, b)))

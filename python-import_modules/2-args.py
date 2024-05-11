5768256ff6bbd93d26e92ae3c981464d40857729
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-11 12:34:57 +0200

    import module

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("0 arguments.")
     else:
-        print(f"{len(sys.argv) - 1} {'argument' if len(sys.argv) - 1 == 1 else 'arguments'}:")
+        if len(sys.argv) - 1 == 1:
+            print("1 argument:")
+        else:
+            print(f"{len(sys.argv) - 1} arguments:")
         for arg_num in range(len(sys.argv) - 1):
             print(f"{arg_num + 1}: {sys.argv[arg_num + 1]}")

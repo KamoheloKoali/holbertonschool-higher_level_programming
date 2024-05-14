b12f55a9dc15db30d5374a4bde0520ce00c0b998
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-14 10:44:02 +0000

    adds 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) > 1 and len(tuple_b) > 1:
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return new_tuple
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     elif len(tuple_a) == 1 and len(tuple_b) > 1:
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_b[1])
-        return new_tuple
+        return (tuple_a[0] + tuple_b[0], tuple_b[1])
     elif len(tuple_a) > 1 and len(tuple_b) == 1:
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1])
-        return new_tuple
-    new_tuple = (0, 0)
-    return new_tuple
+        return (tuple_a[0] + tuple_b[0], tuple_a[1])
+    elif len(tuple_a) < 1 and len(tuple_b) > 1:
+        return tuple_b
+    elif len(tuple_a) > 1 and len(tuple_b) < 1:
+        return tuple_a
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        return (tuple_a[0] + tuple_b[0], 0)
+    return (0, 0)

0c7efb05e70dabff8e6135488e694cc69021a714
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-14 10:13:07 +0000

    prints a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element))
-            if element == len(row) - 1:
+            print("{:d}".format(element), end="")
+            if row.index(element) == len(row) - 1:
                 print()
             else:
-                print("", end=" ")
-        print()
+                print(" ", end="")

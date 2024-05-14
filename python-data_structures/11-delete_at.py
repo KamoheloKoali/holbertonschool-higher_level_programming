0a164adca36f759f83912550acc414db9ab95026
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-14 11:33:43 +0000

    finds all multiples of 2 in a list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = []
     if not my_list:
         return my_list
     if idx >= 0 and idx < len(my_list):
-        for item in my_list:
-            if item != my_list[idx]:
-                new_list.append(item)
-        return new_list
+        return my_list.remove(my_list[idx])
     return my_list

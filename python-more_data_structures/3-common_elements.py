c3b6e2eb006176381e9324d886dc27ed24fec271
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-17 09:32:19 +0000

    returns a set of common elements in two sets

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     new_set = set()
-    for item in set1:
-        if item in set2:
+    for item in set_1:
+        if item in set_2:
             new_set.add(item)
     return new_set

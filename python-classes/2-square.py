3ce9206f997a90cc56e354e06cf6dab39cda9ede
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-19 11:56:53 +0000

    defines a square by: (based on 1-square.py)

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""
+2-square.py: is a class Square that defines a square
+"""
 
+class Square:
+    """Represent a square.
+
+    Attributes:
+        attr1 (_Square_Size): private instance attribute size
+
+    """
+
+    def __init__(self, size=0):
+        """Initializer _square_size"""
+        if size.isdigit():
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size =  size
+        else:
+            raise TypeError("size must be an integer")

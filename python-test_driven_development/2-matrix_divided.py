3912d67d1120c2df026e98bbabb29a608bdbf9f8
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-23 07:50:28 +0000

    fix error handling

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -60,8 +60,6 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         new_row = []
         for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             new_row.append(round(element / div, 2))
         new_matrix.append(new_row)
     return new_matrix

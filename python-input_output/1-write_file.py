d0b87d6863b15e5cdcebb1c7ed65438b8e1d82c5
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-27 12:26:37 +0200

    writes a string to a text file (UTF8)

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+1-write_file.py writes a string to a text file (UTF8)
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8)
+
+    Args:
+        filename (str): name of file to write to
+        text (str): what to write to file
+    """
+
+    written = 0
+    with open(filename, "w",encoding="utf-8") as file:
+        written = file.write(text)
+    return written

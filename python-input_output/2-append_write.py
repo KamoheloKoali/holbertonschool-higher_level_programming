d800b95676b72dd240834c7641c9904da6842be7
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-27 12:34:56 +0200

    appends a string at the end of a text file (UTF8)

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-2-append_write.py appends a string at the end of a text file (UTF8) 
+2-append_write.py appends a string at the end of a text file (UTF8)
 """
 
 
 def append_write(filename="", text=""):
     """
-    Appends a string at the end of a text file (UTF8) 
+    Appends a string at the end of a text file (UTF8)
 
     Args:
         filename (str): name of the file to write to

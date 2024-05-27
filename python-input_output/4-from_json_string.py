6814807318a2fcad85669461b6af70945ff3eeb6
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-27 12:58:58 +0200

    returns an object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """
 4-from_json_string.py returns an object
 """
@@ -10,13 +10,18 @@ def from_json_string(my_str):
     Returns an object
     
     Args:
-        my_str (str): json string
+        my_str (str): JSON string
 
     Returns:
         an object
     """
-    with open("new_text.txt", "a", encoding="utf-8") as file:
+
+    if not my_str.strip():  # Check if the string is empty or contains only whitespace
+        return []
+
+    with open("new_text.txt", "w", encoding="utf-8") as file:
         file.write(my_str)
-    
+
     with open("new_text.txt", "r", encoding="utf-8") as file:
         return json.load(file)
+

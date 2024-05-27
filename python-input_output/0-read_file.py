b9dc5751df72a1818c75e8b2bc1163cbb880513d
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-27 12:13:51 +0200

    reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+0-read_file.py reads a text file (UTF8) and prints it to stdout
+"""
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str): name of file to read
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())

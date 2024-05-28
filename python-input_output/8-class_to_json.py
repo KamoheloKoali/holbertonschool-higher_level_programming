4a02b4cab8faddfb504e58addcb4bbe876c6a42e
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-28 10:43:43 +0200

    class to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+8-class_to_json.py returns the dictionary description with
+simple data structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with
+    simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+
+    Args:
+        obj (class object): object
+    
+    Returns:
+        Returns the dictionary description with
+        simple data structure (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object
+    """
+
+    return obj.__dict__

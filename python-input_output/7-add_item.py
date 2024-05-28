54ce2f52339f2ae2a5bb9659d23d3d15b6aae7a0
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-28 10:22:43 +0200

    adds all arguments to a Python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """
-7-add_item.py adds all arguments to a Python list
+7-add_item.py  adds all arguments to a Python list
 """
-import json
-import sys
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
 
-with open(sys.argv[1], "r") as file:
-    save(load(file), "add_item.json")
+import sys
+import os
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+# Load existing items from the file if it exists
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+# Add all command-line arguments to the list
+items.extend(sys.argv[1:])
+
+# Save the updated list to the file
+save_to_json_file(items, filename)

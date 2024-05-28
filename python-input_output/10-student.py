6577778530ce8271da57a447061632f5abb3019a
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-28 11:20:25 +0200

    defines a student class and retrieves a dictionary representation of a Student instance

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,6 +3,7 @@
 9-student.py defines a student class
 """
 
+
 class Student:
     """
     Defines a student
@@ -18,12 +19,10 @@ class Student:
         age (int): age of the student
     """
 
-
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self, attrs=None):
         """

3c6edd74570a1d56ef1e7cbb4ff5c4462f24c267
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-28 09:46:55 +0200

    writes an Object to a text file, using a JSON representation

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+5-save_to_json_file.py writes an Object to a text file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an Object to a text file, using a JSON representation
+    
+    Args:
+        my_obj (object): python object
+        filename (str): name of the file to write to
+    """
+
+    with open(filename, "w"):
+        json.dump(my_obj, filename)

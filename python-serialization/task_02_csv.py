e9d20d25f8a4eb7f57e3dd5f28ea8e9ac43d7b72
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-05-29 13:04:24 +0200

    reading data from one format (CSV) and converting it into another format (JSON) using serialization techniques

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -13,6 +13,9 @@ def convert_csv_to_json(csv_file):
 
     Args:
         csv_file (str): name of the csv file
+
+    Returns:
+        bool: True if conversion was successful, False otherwise
     """
 
     data = []
@@ -20,14 +23,15 @@ def convert_csv_to_json(csv_file):
     try:
         with open(csv_file, encoding="utf-8") as csvfile:
             csv_read = csv.DictReader(csvfile)
-    except:
-        return false
-    for row in csv_read:
-        data.append(row)
-    
+            for row in csv_read:
+                data.append(row)
+
+    except FileNotFoundError:
+        return False
+
     try:
-        with open("data.json", "w", encoding="utf-8") as file:
-            json.dump(data, file)
+        with open("data.json", "w", encoding="utf-8") as jsonfile:
+            json.dump(data, jsonfile, ensure_ascii=False, indent=4)
     except:
         return False
     return True
